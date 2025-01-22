d7b7a95c47522a591e3c67d65c39f34b44a6b2fb
pabloReyes <andresreyes.ceo@gmail.com>
2025-01-22 16:24:36 -0500

    terminando proyecto amigo secreto

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 let amigosIngresados = [];
 
 function agregarAmigo(){
+    const arrayListaAmigosIngresados = document.getElementById("listaAmigos");
+    arrayListaAmigosIngresados.innerHTML = ""
     let amigoAgregado = document.getElementById("amigo").value
     if (amigoAgregado.length == 0) {
         alert("Por favor, inserte un nombre.")
@@ -9,7 +11,26 @@ function agregarAmigo(){
     }else{
         amigosIngresados.push(amigoAgregado)
         document.getElementById("amigo").value=""
+        for (const amigo of amigosIngresados) {
+            const etiqueta_Li = document.createElement("li")
+            etiqueta_Li.textContent = amigo
+            arrayListaAmigosIngresados.appendChild(etiqueta_Li)
+        }
     }
     return console.log(amigosIngresados);
     
 }
+
+function sortearAmigo() {
+    if (amigosIngresados < 1){
+        alert("no hay usuarios en la base de datos!!")
+        return
+    }
+    
+    let sorteo = Math.floor(Math.random() * amigosIngresados.length);
+    document.getElementById("seleccionado").innerHTML = `tu amigo secreto es: ${amigosIngresados[sorteo]}`
+    
+    return console.log("el num aleatoreo es: "+sorteo+ " el amigo elegido es: "+amigosIngresados[sorteo]);
+}
+
+
